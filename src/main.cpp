00afbee050c2f7204cde87b3aa84368ea4fe9e01
alexchuber <91097647+alexchuber@users.noreply.github.com>
2024-02-09 20:30:17 -0500

    Clean up main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,30 @@
 #include "imgui.h"
 #include "imgui-SFML.h"
-
 #include "SFML/Graphics.hpp"
-#include <box2d/box2d.h>
-#include <iostream>
+#include "box2d/box2d.h"
 
-void DrawPropertyWindow();
-
-int main()
-{
-    // Initialize
+int main() {
+    // Initialize SFML window
     sf::RenderWindow window(sf::VideoMode(1366, 768), "Gator Engine");
     window.setFramerateLimit(60);
+
+    // Initialize ImGui
     ImGui::SFML::Init(window);
     sf::Clock deltaClock;
 
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         // Process user events
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             ImGui::SFML::ProcessEvent(window, event);
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
         }
 
         // Draw GUIs
         ImGui::SFML::Update(window, deltaClock.restart());
-        DrawPropertyWindow();
-
+        
         // Render updated window
         window.clear();
         ImGui::SFML::Render(window);
@@ -39,11 +34,4 @@ int main()
     // Clean up
     ImGui::SFML::Shutdown();
     return 0;
-}
-
-void DrawPropertyWindow()
-{
-	ImGui::Begin("Properties");
-	ImGui::Text("Placeholder for properties stuff");
-	ImGui::End();
 }
