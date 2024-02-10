bbde6a4267e3870dc37ff238d356070f3fef29ca
alexchuber <91097647+alexchuber@users.noreply.github.com>
2024-02-09 20:25:28 -0500

    Add Window class to Editor

---
+++ src/editor/Window.cpp
@@ -0,0 +1,33 @@
+#include "Window.h"
+
+Window::Window() {
+    name_ = "Name me!";
+    window_flags_ = 0;
+
+    // TODO: Elevate this to higher-level class once we have one
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.WindowBorderSize = 0.0f;
+    style.FrameBorderSize = 1.0f;
+    style.ScrollbarRounding = 2.0f;
+    style.GrabRounding = 2.0f;
+    ImGui::StyleColorsLight();
+}
+
+void Window::Draw() {
+    SetPosition();
+    PreDraw();
+    ImGui::Begin(name_.c_str(), NULL, window_flags_);
+    DrawFrames();
+    ImGui::End();
+    PostDraw();
+}
+
+void Window::PreDraw(){
+	// No default implementation; override if needed
+    // I didn't want to make it pure virtual because it's not always needed,
+    // so why force y'all to implement it?
+}
+
+void Window::PostDraw() {
+	// Ditto
+}
---
+++ src/editor/Window.h
@@ -0,0 +1,52 @@
+/*
+	File: Window.h
+
+	Base class for all windows comprising the app's Editor. Provides a common interface
+	for these windows to be drawn. Also allows for polymorphic storage of windows.
+
+	Pure virtual functions are provided for necessary customizations of each window, 
+	such as positioning and drawing. Non-pure virtual functions are provided for 
+	optional customizations, such as any extra setup or cleanup required for drawing.
+
+	Some default, global ImGui customization options are also set here. This can help 
+	ensure a consistent look and feel across all windows. However, these options can
+	be overridden by derived classes.
+*/
+
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <string>
+
+#include "imgui.h"
+
+class Window {
+ public:
+	Window();
+	Window(const Window&) = delete;
+	void operator=(const Window&) = delete;
+
+	// Main draw call for windows. Calls all virtual functions in correct order.
+	virtual void Draw() final;
+
+ protected:
+	// Set the Window's position and size. Used for responsive app resizing.
+	virtual void SetPosition() = 0;
+
+	// Perform any extra setup wanted (positioning, styling) before DrawFrames()
+	virtual void PreDraw();
+
+	// Draw the frames (aka, the inside content) of the window.
+	virtual void DrawFrames() = 0;
+
+	// Perform any cleanup necessary after DrawFrames()
+	virtual void PostDraw();
+
+	// The name of the window. Used for the title bar.
+	std::string name_;
+
+	// ImGui options for the window. 
+	ImGuiWindowFlags window_flags_;
+};
+
+#endif // WINDOW_H
