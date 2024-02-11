2afbfb8fa58d89ed2637bfdafb95b7bb1e0fcb5f
BaronSwango <baronswango30@gmail.com>
2024-02-11 13:29:27 -0500

    Implemented the start of DrawFileBar and DrawTabBar functions, adjusted gitignore, added assets folder with icon assets

--- src/main.cpp
+++ src/main.cpp
@@ -3,113 +3,111 @@
 
 #include "SFML/Graphics.hpp"
 #include <box2d/box2d.h>
-#include <iostream>
-// #include "glm.hpp"
+
+#include <string>
+#include <vector>
+
+void DrawFileBar()
+{
+    if (ImGui::BeginMainMenuBar())
+    {
+        if (ImGui::BeginMenu("File"))
+        {
+            ImGui::MenuItem("Open");
+            ImGui::MenuItem("Save");
+            ImGui::EndMenu();
+        }
+
+        if (ImGui::BeginMenu("..."))
+        {
+            ImGui::MenuItem("Stuff Here");
+            ImGui::EndMenu();
+        }
+        ImGui::EndMainMenuBar();
+    }
+}
+
+void DrawTabBar(sf::RenderWindow &window, std::vector<sf::Texture> &textures)
+{
+    ImGui::SetNextWindowSize(ImVec2(window.getSize().x, 150));
+    ImGui::SetNextWindowPos(ImVec2(0, 20));
+    ImGui::Begin("My Tabs", NULL, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize);
+    if (ImGui::BeginTabBar("MainTabs"))
+    {
+        if (ImGui::BeginTabItem("Sprites"))
+        {
+            // Select button
+            ImGui::SetCursorPos(ImVec2(window.getSize().x / 20, 50));
+            if (ImGui::ImageButton("Select Button", textures[0], sf::Vector2f(60, 60)))
+            {
+                // do stuff if button clicked
+            }
+            ImGui::SetCursorPos(ImVec2(window.getSize().x / 20 + 13, 118));
+            ImGui::Text("Select");
+
+            // Sprite button
+            ImGui::SetCursorPos(ImVec2(window.getSize().x / 3, 50));
+            if (ImGui::ImageButton("Sprite Button", textures[1], sf::Vector2f(60, 60)))
+            {
+                // do stuff if button clicked
+            }
+            ImGui::SetCursorPos(ImVec2(window.getSize().x / 3 + 13, 118));
+            ImGui::Text("Sprite");
+
+            // Game Object button
+            ImGui::SetCursorPos(ImVec2(window.getSize().x / 3 + 90, 50));
+            if (ImGui::ImageButton("Game Object Button", textures[2], sf::Vector2f(60, 60)))
+            {
+                // do stuff if button clicked
+            }
+            ImGui::SetCursorPos(ImVec2(window.getSize().x / 3 + 86, 118));
+            ImGui::Text("Game Object");
+            ImGui::EndTabItem();
+        }
+
+        if (ImGui::BeginTabItem("Testing"))
+        {
+            // additional UI elements specific to testing
+            ImGui::EndTabItem();
+        }
+
+        ImGui::EndTabBar();
+    }
+    ImGui::End();
+}
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1366, 768), "Gator Engine");
-    // glm::bvec4 testVec(1, 0, 3, 4);
     ImGui::SFML::Init(window);
     sf::Clock deltaClock;
 
-    // Create world
-    b2Vec2 gravity(0.0f, -10.0f);
-    b2World world(gravity);
-    // Create a body def for the ground
-    b2BodyDef groundBodyDef;
-    groundBodyDef.position.Set(500, 500);
-    // Create a body using the def for the ground
-    b2Body *groundBody = world.CreateBody(&groundBodyDef);
-    // Create a polygon shape for the ground
-    b2PolygonShape groundBox;
-    groundBox.SetAsBox(50.0f, 50.0f);
-    // Give the ground physical properties
-    groundBody->CreateFixture(&groundBox, 0.0f);
-    // Create the sprite for the ground
-    sf::RectangleShape groundShape(sf::Vector2f(100.0f, 100.0f));
-    groundShape.setPosition(500, window.getSize().y - groundBody->GetPosition().y);
-    groundShape.setOrigin(sf::Vector2f(50, 50));
-    groundShape.setFillColor(sf::Color::Blue);
+    sf::Texture selectIcon;
+    selectIcon.loadFromFile("../assets/SelectIcon.png");
 
-    // Create a body def for the rigid body
-    b2BodyDef bodyDef;
-    bodyDef.type = b2_dynamicBody;
-    bodyDef.position.Set(500, 900);
-    // Create a body using the def for the rigid body
-    b2Body *body = world.CreateBody(&bodyDef);
-    // Create a polygon shape for the rigid body
-    b2PolygonShape dynamicBox;
-    dynamicBox.SetAsBox(5, 5);
-    // Give the rigid body physical properties
-    b2FixtureDef fixtureDef;
-    fixtureDef.shape = &dynamicBox;
-    fixtureDef.density = 1.0f;
-    fixtureDef.friction = 0.3f;
-    body->CreateFixture(&fixtureDef);
+    sf::Texture spriteIcon;
+    spriteIcon.loadFromFile("../assets/SpriteIcon.png");
 
-    // Create the sprite for the rigid body
-    sf::RectangleShape boxShape(sf::Vector2f(10, 10));
-    boxShape.setPosition(500, window.getSize().y - body->GetPosition().y);
-    boxShape.setRotation(body->GetAngle());
-    boxShape.setOrigin(sf::Vector2f(5, 5));
-    boxShape.setFillColor(sf::Color::Red);
+    sf::Texture gameObjectIcon;
+    gameObjectIcon.loadFromFile("../assets/GameObjectIcon.png");
 
-    // Data for physics simulation steps
-    float timeStep = 1.0f / 60.0f;
-    int32 velocityIterations = 6;
-    int32 positionIterations = 2;
-
-    window.setFramerateLimit(60);
-    // Attach
-    float redBoxLength = 10;
-    float redBoxHeight = 10;
-    float redBoxX = 500;
-    float redBoxY = 768;
+    std::vector<sf::Texture> icons = {selectIcon, spriteIcon, gameObjectIcon};
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // Process ImGui gui events
             ImGui::SFML::ProcessEvent(event);
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        // Update the ImGui visuals
         ImGui::SFML::Update(window, deltaClock.restart());
-        ImGui::Begin("Red Square Size");
-        ImGui::Text("Drag the slider below to control red box size and position");
-        ImGui::SliderFloat("Length", &redBoxLength, 10, 300);
-        ImGui::SliderFloat("Height", &redBoxHeight, 10, 300);
 
-        // Update physics objects based on gui's
-        dynamicBox.SetAsBox(redBoxLength / 2, redBoxHeight / 2); // Updating size
-        // The position should only be updated if there is a change in the XPos/YPos slider
-        if (ImGui::SliderFloat("XPos", &redBoxX, 0, 1366))
-        {
-            body->SetTransform(b2Vec2(redBoxX, body->GetPosition().y), body->GetAngle()); // Updating x position
-            body->SetAwake(true);
-        }
+        DrawFileBar();
+        DrawTabBar(window, icons);
 
-        if (ImGui::SliderFloat("YPos", &redBoxY, 0, 768))
-        {
-            body->SetTransform(b2Vec2(body->GetPosition().x, redBoxY), body->GetAngle()); // Updating y position
-            body->SetAwake(true);
-        }
-        ImGui::End();
-        // Run a physics step at 1/60 of a second
-        world.Step(timeStep, velocityIterations, positionIterations);
-        // Update sprite positions and rotations and sizes based on physics objects
-        boxShape.setPosition(body->GetPosition().x, window.getSize().y - body->GetPosition().y);
-        boxShape.setRotation(body->GetAngle());
-        boxShape.setSize(sf::Vector2f(redBoxLength, redBoxHeight));
-        boxShape.setOrigin(redBoxLength / 2, redBoxHeight / 2);
-
-        window.clear();
-        window.draw(groundShape);
-        window.draw(boxShape);
+        window.clear(sf::Color(0, 0, 0));
         ImGui::SFML::Render(window);
         window.display();
     }
