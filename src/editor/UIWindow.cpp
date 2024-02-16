7e32fff32ce7d34c4aad94ee00d9a3ddff9be51d
alexchuber <91097647+alexchuber@users.noreply.github.com>
2024-02-16 16:48:29 -0500

    Remove comment

--- src/editor/UIWindow.cpp
+++ src/editor/UIWindow.cpp
@@ -4,6 +4,8 @@ std::shared_ptr<Entity> UIWindow::active_entity_ = nullptr;
 
 UIWindow::UIWindow() {
 	name_ = "Name me!";
+
+	// Disallow moving windows by default
 	window_flags_ = 0;
 	window_flags_ |= ImGuiWindowFlags_NoCollapse;
 	window_flags_ |= ImGuiWindowFlags_NoMove;
