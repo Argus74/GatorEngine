8fa0d4324c3393cbb9b8c93b3d3ee6a6276c4fa5
lr2120 <lr2120@gmail.com>
2024-03-12 12:45:41 -0400

    Partial changes.

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -1,41 +1,51 @@
 #include "Animation.h"
+#include <cmath>
 
-Animation::Animation() {
-
-};
-
-Animation::Animation(const std::string& name, const sf::Texture& texture)
-	: Animation(name, texture, 1, 0)
+Animation::Animation()
 {
+}
 
-};
-
-Animation::Animation(const std::string& name, const sf::Texture& texture, size_t frameCount, size_t animationSpeed) 
-: name_(name),
-sprite_(texture),
-frameCount_(frameCount),
-currentFrame_(0),
-speed_(animationSpeed)
+Animation::Animation(const std::string &name, const sf::Texture &t)
+	: Animation(name, t, 1, 0)
 {
-    size_ = Vec2((float)texture.getSize().x / frameCount, (float)texture.getSize().y);
-    sprite_.setOrigin(size_.x / 2.0f, size_.y / 2.0f);
-    sprite_.setTextureRect(sf::IntRect(std::floor(currentFrame_) * size_.x, 0, size_.x, size_.y)); //Setting the texture of our Rectangle to be the first frame/current
-};
+}
 
-void Animation::Update() {
-    // Increase the current frame based on the speed
-    currentFrame_ += speed_;
+Animation::Animation(const std::string &name, const sf::Texture &t, size_t frameCount, size_t speed)
+	: m_name(name), m_sprite(t), m_frameCount(frameCount), m_currentFrame(0), m_speed(speed)
+{
+	m_size = Vec2(float)t.getSize().x / frameCount, (float)t.getSize().y);
+	m_sprite.setOrigin(m_size.x / 2.0f, m_size.y / 2.0f);
+	m_sprite.setTextureRect(sf::IntRect(std::floor(m_currentFrame) * m_size.x, 0, m_size.x, m_size.y));
+}
 
-    // Check if the animation has reached the end
-    if (currentFrame_ >= frameCount_) {
-        currentFrame_ = 0;
-        reachedEnd_ = true;
-    }
+// updates the animation to show the next frame, depending on its speed
+// animation loops when it reaches the end
+void Animation::update()
+{
+	// add the speed variable to the current frame
+	m_currentFrame++;
 
-    // Calculating the position of the current frame within the texture
-    float framePosition = currentFrame_ * size_.x;
+	// TODO: 1) calculate the current frame of animation to play based on currentFrame and speed
+	//		 2) set the texture rectangle properly (see constructor for sample)
+}
 
-    // Update the texture rectangle to display the current frame
-    sprite_.setTextureRect(sf::IntRect(framePosition, 0, size_.x, size_.y));
-};
+const Vec2 &Animation::getSize() const
+{
+	return m_size;
+}
 
+const std::string &Animation::getName() const
+{
+	return m_name;
+}
+
+sf::Sprite &Animation::getSprite()
+{
+	return m_sprite;
+}
+
+bool Animation::hasEnded() const
+{
+	// TODO: detect when animation has ended (last frame was played) and return true
+	return false;
+}
--- src/Animation.h
+++ src/Animation.h
@@ -1,24 +1,27 @@
-#include <SFML/Graphics/Texture.hpp>
-#include <SFML/Graphics/Sprite.hpp>
-#include <cmath>
-#include "Vec2.h"
-#include <iostream>
+#pragma once
 
-class Animation {
+#include "Vec2.h"
+
+#include <vector>
+#include <SFML/Graphics.hpp>
+
+class Animation
+{
+	sf::Sprite m_sprite;
+	size_t m_frameCount = 1;   // total number of frames of animation
+	size_t m_currentFrame = 0; // the current frame of animation being played
+	size_t m_speed = 0;		   // the speed to play this animation
+	Vec2 m_size = {1, 1};	   // size of the animation frame
+	std::string m_name = "none";
+
 public:
 	Animation();
-	Animation(const std::string& name, const sf::Texture &texture);
-	Animation(const std::string& name, const sf::Texture &texture, size_t frameCount, size_t animationSpeed);
-	
+	Animation(const std::string &name, const sf::Textuer &t);
+	Animation(const std::string &name, const sf::Texture &t, size_t frameCount, size_t speed);
 
-	void Update();
-
-	std::string name_ = "none";
-	sf::Sprite sprite_; // Texture the animation gets frames from
-	size_t frameCount_ = 1;
-	size_t speed_ = 0; // Speed of animation
-	size_t currentFrame_ = 0; //Current frame of the animation
-	Vec2 size_ = { 1, 1 }; // (Width of a of the frame animation, height of a of the frame animation)
-	bool reachedEnd_ = false;
-	
+	void update();
+	bool hasEnded() const;
+	const std::string &getName() const;
+	const Vec2 &getSize() const;
+	sf::Sprite &getSprite();
 };
