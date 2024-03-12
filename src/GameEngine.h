9469deb89c50d743c8806f786cf079991b1c1763
lr2120 <lr2120@gmail.com>
2024-03-12 12:00:19 -0400

    Dvides Game class into GameEngine and the different Scene class.

---
+++ src/GameEngine.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "Scene.h"
+#include "Assets.h"
+
+#include <memory>
+
+typedef std::map<std::string, std::shared_ptr<Scene>> SceneMap;
+
+class GameEngine
+{
+
+protected:
+
+	sf::RenderWindow	m_window;
+	Assets				m_assets;
+	std::string			m_currentScene;
+	SceneMap			m_sceneMap;
+	size_t				m_simulationSpeed = 1;
+	bool				m_running = true;
+
+	void				init(const std::string& path);
+	void				update();
+
+	void				sUserInput();
+
+	std::shared_ptr<Scene> currentScene();
+
+public:
+
+	GameEngine(const std::string& path);
+
+	void changeScene(const std::string& sceneName, std::shared_ptr<Scene> scene, bool endCurrentScene = false);
+
+	void quit();
+	void run();
+
+	sf::RenderWindow& window();
+	const Assets& assets() const;
+	bool isRunning();
+};
